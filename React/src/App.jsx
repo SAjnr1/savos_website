feda6aae40faee8bdde7e173e1c86e3894eb8f2b
SAjnr1 <solomonagbeko123@gmail.com>
2026-07-20 13:29:27 +0000

    Updated footer

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -7,7 +7,7 @@ import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import Footer from './Components/Footer/footer'
 
 
 const App = () => {
